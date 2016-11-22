019303f72e2d08d015b45aed83063cf5210066c8
Rafal Ganczarek <github@ganczarek.info>
2016-11-22 22:17:28 +0000

    Use ScalaTest with ScalaCheck for better integration with IDE

--- exercises/src/test/scala/fpinscala/monoids/MonoidSpecifications.scala
+++ exercises/src/test/scala/fpinscala/monoids/MonoidSpecifications.scala
@@ -1,37 +1,44 @@
 package fpinscala.monoids
 
-import org.scalacheck.{Gen, Properties}
-import org.scalacheck.Prop.forAll
+import org.scalacheck.Gen
+import org.scalatest.prop._
+import org.scalatest.{Matchers, PropSpec}
 
-class MonoidLawsSpec[T](override val name: String, val monoid: Monoid[T], val gen: Gen[T]) extends Properties(monoid.getClass.getName) {
+abstract class MonoidLawsSpec[T](val monoid: Monoid[T], val gen: Gen[T]) extends PropSpec with PropertyChecks with Matchers {
 
-  property("identity law") = forAll(gen) {
-    (x: T) => monoid.op(x, monoid.zero) == x
+  property("identity law") {
+    forAll(gen) {
+      (x: T) => monoid.op(x, monoid.zero) shouldBe x
+    }
   }
 
-  property("associativity law") = forAll(gen, gen, gen) {
-    (x: T, y: T, z: T) => monoid.op(monoid.op(x, y), z) == monoid.op(x, monoid.op(y, z))
+  property("associativity law") {
+    forAll(gen, gen, gen) {
+      (x: T, y: T, z: T) => monoid.op(monoid.op(x, y), z) shouldBe monoid.op(x, monoid.op(y, z))
+    }
   }
-
 }
 
-class EndofuncitionMonoidLawsSpec[T](override val name: String,
-                                     val monoid: Monoid[T => T],
-                                     val valueGen: Gen[T],
-                                     val endofunctionGen: Gen[T => T]) extends Properties(monoid.getClass.getName) {
+abstract class EndofuncitionMonoidLawsSpec[T](val monoid: Monoid[T => T],
+                                              val valueGen: Gen[T],
+                                              val endofunctionGen: Gen[T => T]) extends PropSpec with PropertyChecks with Matchers {
 
-  property("identity law") = forAll(endofunctionGen) {
-    (f: T => T) => forAll(valueGen) {
-      (x:T) => monoid.op(f, monoid.zero).apply(x) == f.apply(x)
+  property("identity law") {
+    forAll(endofunctionGen) {
+      (f: T => T) => forAll(valueGen) {
+        (x: T) => monoid.op(f, monoid.zero).apply(x) shouldBe f.apply(x)
+      }
     }
   }
 
-  property("associativity law") = forAll(endofunctionGen, endofunctionGen, endofunctionGen) {
-    (f: T => T, g: T => T, h: T => T) => forAll(valueGen) {
-      (x:T) => monoid.op(monoid.op(f, g), h).apply(x) == monoid.op(f, monoid.op(g, h)).apply(x)
+  property("associativity law") {
+    forAll(endofunctionGen, endofunctionGen, endofunctionGen) {
+      (f: T => T, g: T => T, h: T => T) => forAll(valueGen) {
+        (x: T) => monoid.op(monoid.op(f, g), h).apply(x) shouldBe monoid.op(f, monoid.op(g, h)).apply(x)
+      }
     }
-  }
 
+  }
 }
 
 object EndofuncitionMonoidLawsSpec {
@@ -43,17 +50,17 @@ object EndofuncitionMonoidLawsSpec {
 
 }
 
-object IntAdditionSpec extends MonoidLawsSpec[Int]("intAddition", Monoid.intAddition, Gen.choose(Int.MinValue, Int.MaxValue))
+class IntAdditionSpec extends MonoidLawsSpec[Int](Monoid.intAddition, Gen.choose(Int.MinValue, Int.MaxValue))
 
-object IntMultiplicationSpec extends MonoidLawsSpec[Int]("intMultiplication", Monoid.intMultiplication, Gen.choose(Int.MinValue, Int.MaxValue))
+class IntMultiplicationSpec extends MonoidLawsSpec[Int](Monoid.intMultiplication, Gen.choose(Int.MinValue, Int.MaxValue))
 
-object BooleanOrSpec extends MonoidLawsSpec[Boolean]("booleanOr", Monoid.booleanOr, Gen.oneOf(true, false))
+class BooleanOrSpec extends MonoidLawsSpec[Boolean](Monoid.booleanOr, Gen.oneOf(true, false))
 
-object BolleanAndSpec extends MonoidLawsSpec[Boolean]("booleanAnd", Monoid.booleanAnd, Gen.oneOf(true, false))
+class BolleanAndSpec extends MonoidLawsSpec[Boolean](Monoid.booleanAnd, Gen.oneOf(true, false))
 
-object OptionIntMonoid extends MonoidLawsSpec[Option[Int]]("optionIntMonoid", Monoid.optionMonoid, Gen.option(Gen.choose(Int.MinValue, Int.MaxValue)))
+class OptionIntMonoid extends MonoidLawsSpec[Option[Int]](Monoid.optionMonoid, Gen.option(Gen.choose(Int.MinValue, Int.MaxValue)))
 
-object OptionBoolMonoid extends MonoidLawsSpec[Option[Boolean]]("optionBoolMonoid", Monoid.optionMonoid, Gen.option(Gen.oneOf(true, false)))
+class OptionBoolMonoid extends MonoidLawsSpec[Option[Boolean]](Monoid.optionMonoid, Gen.option(Gen.oneOf(true, false)))
 
-object EndofunctionMonoid extends EndofuncitionMonoidLawsSpec[Int]("endoIntMonoid", Monoid.endoMonoid,
-  Gen.choose(Int.MinValue, Int.MaxValue), EndofuncitionMonoidLawsSpec.linearFunctionGen)
+class EndofunctionMonoid extends EndofuncitionMonoidLawsSpec[Int](
+  Monoid.endoMonoid, Gen.choose(Int.MinValue, Int.MaxValue), EndofuncitionMonoidLawsSpec.linearFunctionGen)
