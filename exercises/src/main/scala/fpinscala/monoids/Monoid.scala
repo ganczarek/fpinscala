d410bec6c95642e4dde9674f4a68ecc76d7d6854
Rafal Ganczarek <github@ganczarek.info>
2016-11-22 21:50:03 +0000

    Finished Exercise 10.3 and 10.4

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -1,11 +1,11 @@
 package fpinscala.monoids
 
-import fpinscala.parallelism.Nonblocking._
-import fpinscala.parallelism.Nonblocking.Par.toParOps // infix syntax for `Par.map`, `Par.flatMap`, etc
-import language.higherKinds
+// infix syntax for `Par.map`, `Par.flatMap`, etc
+import scala.language.higherKinds
 
 trait Monoid[A] {
   def op(a1: A, a2: A): A
+
   def zero: A
 }
 
@@ -13,103 +13,108 @@ object Monoid {
 
   val stringMonoid = new Monoid[String] {
     def op(a1: String, a2: String) = a1 + a2
+
     val zero = ""
   }
 
   def listMonoid[A] = new Monoid[List[A]] {
     def op(a1: List[A], a2: List[A]) = a1 ++ a2
+
     val zero = Nil
   }
 
   val intAddition: Monoid[Int] = new Monoid[Int] {
     override def op(a1: Int, a2: Int): Int = a1 + a2
+
     override def zero: Int = 0
   }
 
   val intMultiplication: Monoid[Int] = new Monoid[Int] {
     override def op(a1: Int, a2: Int): Int = a1 * a2
+
     override def zero: Int = 1
   }
 
   val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
     override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+
     override def zero: Boolean = false
   }
 
   val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
     override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
     override def zero: Boolean = true
   }
 
   def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
     override def op(a1: Option[A], a2: Option[A]): Option[A] = a1 orElse a2
+
     override def zero: Option[A] = None
   }
 
+  def endoMonoid[A]: Monoid[A => A] = new Monoid[(A) => A] {
+    override def op(f: (A) => A, g: (A) => A): (A) => A = f andThen g
+
+    override def zero: (A) => A = Predef.identity
+  }
+
+  // For monoidLaws see ScalaCheck properties in MonoidSpecification.scala
+  //def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop = ???
+
   /*
-    def endoMonoid[A]: Monoid[A => A] = sys.error("todo")
+  def trimMonoid(s: String): Monoid[String] = sys.error("todo")
 
-    // TODO: Placeholder for `Prop`. Remove once you have implemented the `Prop`
-    // data type from Part 2.
-    trait Prop {}
+  def concatenate[A](as: List[A], m: Monoid[A]): A =
+    sys.error("todo")
 
-    // TODO: Placeholder for `Gen`. Remove once you have implemented the `Gen`
-    // data type from Part 2.
+  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B =
+    sys.error("todo")
 
-    import fpinscala.testing._
-    import Prop._
-    def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop = sys.error("todo")
+  def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B =
+    sys.error("todo")
 
-    def trimMonoid(s: String): Monoid[String] = sys.error("todo")
+  def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B =
+    sys.error("todo")
 
-    def concatenate[A](as: List[A], m: Monoid[A]): A =
-      sys.error("todo")
+  def foldMapV[A, B](as: IndexedSeq[A], m: Monoid[B])(f: A => B): B =
+    sys.error("todo")
 
-    def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B =
-      sys.error("todo")
+  def ordered(ints: IndexedSeq[Int]): Boolean =
+    sys.error("todo")
 
-    def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B =
-      sys.error("todo")
+  sealed trait WC
 
-    def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B =
-      sys.error("todo")
+  case class Stub(chars: String) extends WC
 
-    def foldMapV[A, B](as: IndexedSeq[A], m: Monoid[B])(f: A => B): B =
-      sys.error("todo")
+  case class Part(lStub: String, words: Int, rStub: String) extends WC
 
-    def ordered(ints: IndexedSeq[Int]): Boolean =
-      sys.error("todo")
+  def par[A](m: Monoid[A]): Monoid[Par[A]] =
+    sys.error("todo")
 
-    sealed trait WC
-    case class Stub(chars: String) extends WC
-    case class Part(lStub: String, words: Int, rStub: String) extends WC
+  def parFoldMap[A, B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): Par[B] =
+    sys.error("todo")
 
-    def par[A](m: Monoid[A]): Monoid[Par[A]] =
-      sys.error("todo")
+  val wcMonoid: Monoid[WC] = sys.error("todo")
 
-    def parFoldMap[A,B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): Par[B] =
-      sys.error("todo")
+  def count(s: String): Int = sys.error("todo")
 
-    val wcMonoid: Monoid[WC] = sys.error("todo")
+  def productMonoid[A, B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] =
+    sys.error("todo")
 
-    def count(s: String): Int = sys.error("todo")
+  def functionMonoid[A, B](B: Monoid[B]): Monoid[A => B] =
+    sys.error("todo")
 
-    def productMonoid[A,B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] =
-      sys.error("todo")
+  def mapMergeMonoid[K, V](V: Monoid[V]): Monoid[Map[K, V]] =
+    sys.error("todo")
 
-    def functionMonoid[A,B](B: Monoid[B]): Monoid[A => B] =
-      sys.error("todo")
+  def bag[A](as: IndexedSeq[A]): Map[A, Int] =
+    sys.error("todo")
 
-    def mapMergeMonoid[K,V](V: Monoid[V]): Monoid[Map[K, V]] =
-      sys.error("todo")
-
-    def bag[A](as: IndexedSeq[A]): Map[A, Int] =
-      sys.error("todo")
   */
 }
 
 trait Foldable[F[_]] {
-  import Monoid._
 
   def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B =
     sys.error("todo")
@@ -130,8 +135,10 @@ trait Foldable[F[_]] {
 object ListFoldable extends Foldable[List] {
   override def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B) =
     sys.error("todo")
+
   override def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B) =
     sys.error("todo")
+
   override def foldMap[A, B](as: List[A])(f: A => B)(mb: Monoid[B]): B =
     sys.error("todo")
 }
@@ -139,8 +146,10 @@ object ListFoldable extends Foldable[List] {
 object IndexedSeqFoldable extends Foldable[IndexedSeq] {
   override def foldRight[A, B](as: IndexedSeq[A])(z: B)(f: (A, B) => B) =
     sys.error("todo")
+
   override def foldLeft[A, B](as: IndexedSeq[A])(z: B)(f: (B, A) => B) =
     sys.error("todo")
+
   override def foldMap[A, B](as: IndexedSeq[A])(f: A => B)(mb: Monoid[B]): B =
     sys.error("todo")
 }
@@ -148,19 +157,24 @@ object IndexedSeqFoldable extends Foldable[IndexedSeq] {
 object StreamFoldable extends Foldable[Stream] {
   override def foldRight[A, B](as: Stream[A])(z: B)(f: (A, B) => B) =
     sys.error("todo")
+
   override def foldLeft[A, B](as: Stream[A])(z: B)(f: (B, A) => B) =
     sys.error("todo")
 }
 
 sealed trait Tree[+A]
+
 case class Leaf[A](value: A) extends Tree[A]
+
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object TreeFoldable extends Foldable[Tree] {
   override def foldMap[A, B](as: Tree[A])(f: A => B)(mb: Monoid[B]): B =
     sys.error("todo")
+
   override def foldLeft[A, B](as: Tree[A])(z: B)(f: (B, A) => B) =
     sys.error("todo")
+
   override def foldRight[A, B](as: Tree[A])(z: B)(f: (A, B) => B) =
     sys.error("todo")
 }
@@ -168,8 +182,10 @@ object TreeFoldable extends Foldable[Tree] {
 object OptionFoldable extends Foldable[Option] {
   override def foldMap[A, B](as: Option[A])(f: A => B)(mb: Monoid[B]): B =
     sys.error("todo")
+
   override def foldLeft[A, B](as: Option[A])(z: B)(f: (B, A) => B) =
     sys.error("todo")
+
   override def foldRight[A, B](as: Option[A])(z: B)(f: (A, B) => B) =
     sys.error("todo")
 }
--- exercises/src/test/scala/fpinscala/monoids/MonoidSpecifications.scala
+++ exercises/src/test/scala/fpinscala/monoids/MonoidSpecifications.scala
@@ -5,18 +5,55 @@ import org.scalacheck.Prop.forAll
 
 class MonoidLawsSpec[T](override val name: String, val monoid: Monoid[T], val gen: Gen[T]) extends Properties(monoid.getClass.getName) {
 
-  property("identity law") = forAll (gen) {
+  property("identity law") = forAll(gen) {
     (x: T) => monoid.op(x, monoid.zero) == x
   }
 
-  property("associativity law") = forAll (gen, gen, gen) {
+  property("associativity law") = forAll(gen, gen, gen) {
     (x: T, y: T, z: T) => monoid.op(monoid.op(x, y), z) == monoid.op(x, monoid.op(y, z))
   }
+
+}
+
+class EndofuncitionMonoidLawsSpec[T](override val name: String,
+                                     val monoid: Monoid[T => T],
+                                     val valueGen: Gen[T],
+                                     val endofunctionGen: Gen[T => T]) extends Properties(monoid.getClass.getName) {
+
+  property("identity law") = forAll(endofunctionGen) {
+    (f: T => T) => forAll(valueGen) {
+      (x:T) => monoid.op(f, monoid.zero).apply(x) == f.apply(x)
+    }
+  }
+
+  property("associativity law") = forAll(endofunctionGen, endofunctionGen, endofunctionGen) {
+    (f: T => T, g: T => T, h: T => T) => forAll(valueGen) {
+      (x:T) => monoid.op(monoid.op(f, g), h).apply(x) == monoid.op(f, monoid.op(g, h)).apply(x)
+    }
+  }
+
+}
+
+object EndofuncitionMonoidLawsSpec {
+
+  val linearFunctionGen = for {
+    n <- Gen.choose(Int.MinValue, Int.MaxValue)
+    m <- Gen.choose(Int.MinValue, Int.MaxValue)
+  } yield (x: Int) => n * x + m
+
 }
 
 object IntAdditionSpec extends MonoidLawsSpec[Int]("intAddition", Monoid.intAddition, Gen.choose(Int.MinValue, Int.MaxValue))
+
 object IntMultiplicationSpec extends MonoidLawsSpec[Int]("intMultiplication", Monoid.intMultiplication, Gen.choose(Int.MinValue, Int.MaxValue))
+
 object BooleanOrSpec extends MonoidLawsSpec[Boolean]("booleanOr", Monoid.booleanOr, Gen.oneOf(true, false))
+
 object BolleanAndSpec extends MonoidLawsSpec[Boolean]("booleanAnd", Monoid.booleanAnd, Gen.oneOf(true, false))
+
 object OptionIntMonoid extends MonoidLawsSpec[Option[Int]]("optionIntMonoid", Monoid.optionMonoid, Gen.option(Gen.choose(Int.MinValue, Int.MaxValue)))
+
 object OptionBoolMonoid extends MonoidLawsSpec[Option[Boolean]]("optionBoolMonoid", Monoid.optionMonoid, Gen.option(Gen.oneOf(true, false)))
+
+object EndofunctionMonoid extends EndofuncitionMonoidLawsSpec[Int]("endoIntMonoid", Monoid.endoMonoid,
+  Gen.choose(Int.MinValue, Int.MaxValue), EndofuncitionMonoidLawsSpec.linearFunctionGen)
