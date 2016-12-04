059b9c9562e95256f432b40f96cfcdd251356f43
Rafal Ganczarek <github@ganczarek.info>
2016-12-04 11:47:02 +0000

    Exercise 11.5, no description, just tests

--- exercises/src/test/scala/fpinscala/monads/MonadSpec.scala
+++ exercises/src/test/scala/fpinscala/monads/MonadSpec.scala
@@ -22,21 +22,41 @@ class MonadSpec extends FunSpec with Matchers {
   describe("Monadic combinators") {
     it("should traverse") {
       def unit(x: Any) = Monad.optionMonad.unit(x)
-      val list = List(1,2,3,4)
+      val list = List(1, 2, 3, 4)
       Monad.optionMonad.traverse(list)(unit(_)) shouldBe unit(list)
+    }
+
+    it("should traverse list monad") {
+      def unit(x: Any) = Monad.listMonad.unit(x)
+      val list = List(1, 2)
+      Monad.listMonad.traverse(list)(a => List(a, a)) shouldBe List(list, list, list, list)
     }
 
     it("should combine list of monad instances into list monad") {
       def unit(x: Any) = Monad.optionMonad.unit(x)
-      val list = List(unit(1),unit(2),unit(3),unit(4))
-      Monad.optionMonad.sequence(list) shouldBe unit(List(1,2,3,4))
+      val list = List(unit(1), unit(2), unit(3), unit(4))
+      Monad.optionMonad.sequence(list) shouldBe unit(List(1, 2, 3, 4))
     }
 
-    it("should replicate in monad") {
-      def unit(x: Any) = Monad.optionMonad.unit(x)
-      Monad.optionMonad.replicateM(5, unit(1)) shouldBe unit(List(1,1,1,1,1))
-      Monad.optionMonad.replicateM(5, unit(null)) shouldBe unit(null)
+    it("should combine lists of more than one element") {
+      def unit(x: Any) = Monad.listMonad.unit(x)
+      val list = List(List(1, 2), List(2, 3))
+      Monad.listMonad.sequence(list) should contain theSameElementsAs List(List(1, 2), List(1, 3), List(2, 2), List(2, 3))
     }
+
+    it("should replicate in option monad") {
+      def unit(x: Any) = Monad.optionMonad.unit(x)
+      Monad.optionMonad.replicateM(5, unit(1)) shouldBe Some(List(1, 1, 1, 1, 1))
+      Monad.optionMonad.replicateM(5, unit(null)) shouldBe None
+    }
+
+    it("should replicate in list monad") {
+      def unit(x: Any) = Monad.listMonad.unit(x)
+      Monad.listMonad.replicateM(5, unit(1)) shouldBe List(List(1, 1, 1, 1, 1))
+      Monad.listMonad.replicateM(1, List(1, 2)) shouldBe List(List(1), List(2))
+      Monad.listMonad.replicateM(2, List(1, 2)) should contain theSameElementsAs List(List(1, 1), List(1, 2), List(2, 1), List(2, 2))
+    }
+
   }
 
 }
