96b7177e1b4198fe00c263bf877b076d40356b62
Rafal Ganczarek <github@ganczarek.info>
2018-11-08 15:18:31 +0000

    Exercise 3.1

---
+++ exercises/src/test/scala/fpinscala/datastructures/ListSpec.scala
@@ -0,0 +1,17 @@
+package fpinscala.datastructures
+
+import org.scalatest.{FunSpec, Matchers}
+
+class ListSpec extends FunSpec with Matchers {
+
+  describe("Exercise 3.1") {
+    it("should be 3") {
+      import fpinscala.datastructures.List.x
+
+      x shouldBe 3
+    }
+
+    `
+  }
+
+}
