0b18b2a6b57c0ddb2dc1df9e92296683a8b039c1
Rafal Ganczarek <github@ganczarek.info>
2018-11-08 15:25:37 +0000

    Exericse 3.2

--- exercises/src/test/scala/fpinscala/datastructures/ListSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ListSpec.scala
@@ -1,5 +1,6 @@
 package fpinscala.datastructures
 
+import fpinscala.datastructures.List._
 import org.scalatest.{FunSpec, Matchers}
 
 class ListSpec extends FunSpec with Matchers {
@@ -7,11 +8,24 @@ class ListSpec extends FunSpec with Matchers {
   describe("Exercise 3.1") {
     it("should be 3") {
       import fpinscala.datastructures.List.x
-
       x shouldBe 3
     }
+  }
 
-    `
+  describe("Exercise 3.2") {
+    it("tail should do nothing if empty list") {
+      assertThrows[IllegalArgumentException] {
+        tail(List())
+      }
+    }
+
+    it("tail should return empty list for a list with a single element") {
+      tail(List(1)) shouldBe List()
+    }
+
+    it("tail should remove first element form the list") {
+      tail(List(1, 2, 3, 4, 5)) shouldBe List(2, 3, 4, 5)
+    }
   }
 
 }
